e10804fd85a755784590608a9b42946f1a9dbf29
1svxz <princegabrielferrancullo024@gmail.com>
2026-08-22 02:23:21 +0800

    Update Lunex.lua

--- Lunex.lua
+++ Lunex.lua
@@ -2451,7 +2451,7 @@ function Library:CreateMobileToggle(on_toggle)
         Name = "MobileToggle",
         BackgroundTransparency = 1,
         BorderSizePixel = 0,
-        Position = UDim2.new(1, -95, 0, 3.5),  -- right side, slightly upward
+        Position = UDim2.new(1, -60, 0, -20),  -- right side, slightly upward
         Size = UDim2.fromOffset(40, 40),      -- small clickable area
         ClipsDescendants = false,             -- allow logo to overflow
         ZIndex = 600,
@@ -2463,7 +2463,7 @@ function Library:CreateMobileToggle(on_toggle)
         Image = logo_asset or "rbxassetid://1234567890",
         BackgroundTransparency = 1,
         Size = UDim2.fromOffset(135, 135),
-        Position = UDim2.new(1, -95, 0, 3.5),
+        Position = UDim2.new(1, -60, 0, -20),
         AnchorPoint = Vector2.new(0.5, 0.5),
         ZIndex = 601,
         AutoButtonColor = false,
